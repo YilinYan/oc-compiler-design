14a5acc08bf93ea6a75ce01ea877ea6bc8e5951f
yilin <yilinyan1030@gmail.com>
2018-10-06 22:35:21 -0700

    commit

--- asg1/main.cpp
+++ asg1/main.cpp
@@ -1,13 +1,39 @@
 #include "string_set.h"
-#include <cstdio>
+#include <stdio.h>
+#include <string>
+#include <errno.h>
 using namespace std;
 
-int main(){
-	string_set S;
-	S.intern_stringset("hello");
-	S.intern_stringset("hi");
-	S.intern_stringset("hey");
-	FILE* f;
-	f = fopen("out.txt", "r");
-	S.dump(f);
+#define LINESIZE 1024
+const string CPP = "/usr/bin/cpp -nostdinc";
+
+
+char line[LINESIZE], buf[LINESIZE];
+void readlines(FILE* pipe, const char* file) {
+	for(int linenum = 1;; ++linenum) {
+		char* fgets_rc = fgets (buf, LINESIZE, pipe);
+		printf("%d: %s\n", linenum, fgets_rc);
+	}
+}
+
+int main (int argc, char* argv[]) {
+	char* exename = basename (argv[0]);
+	int exit_status = EXIT_SUCCESS;
+	for (int i = 1; i < argc; ++i) {
+		char* file = argv[i];
+		string cmd = CPP + " " + file; 
+		FILE* pipe = popen (cmd.c_str(), "r");
+		if (!pipe) {
+			exit_status = EXIT_FAILURE;
+			fprintf (stderr, "%s: %s: %s\n",
+					exename, cmd.c_str(), strerror(errno));
+		}
+		else {
+			readlines (pipe, file);
+			int pclose_rc = pclose (pipe);
+			// eprint_status (cmd.c_str(), pclose_rc);
+			if (pclose_rc != 0) exit_status = EXIT_FAILURE;
+		}
+	}
+	return exit_status;
 }
